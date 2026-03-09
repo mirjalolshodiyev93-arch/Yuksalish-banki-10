644eb88e25b55cad3b6bb98b395ba7b8ec2fb58e
mirjalol <mirjalolshodiyev93@gmail.com>
2026-03-09 20:24:27 +0300

    dfregrtgggggggggggggg

--- src/data/loans.js
+++ src/data/loans.js
@@ -1,10 +1,12 @@
-export const loans = [
-  { month: "Jan", amount: 2000 },
-  { month: "Mar", amount: 4000 },
-  { month: "May", amount: 6000 },
-  { month: "Jul", amount: 8000 },
-  { month: "Sep", amount: 10000 },
-  { month: "Nov", amount: 12000 },
+export const loans =(t) =>  [
+  { month: t("loans.jan"),
+     amount: 2000 
+  },
+  { month: t("loans.mar"), amount: 4000 },
+  { month: t("loans.may"), amount: 3000 },
+  { month: t("loans.jul"), amount: 6000 },
+  { month: t("loans.sep"), amount: 5000 },
+  { month: t("loans.nov"), amount: 9000 },
 ];
 
 
